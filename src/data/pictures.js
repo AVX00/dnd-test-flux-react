508b5ff4774a83710ca32879aab168f511e0572a
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-08 12:18:23 +0100

    added a list so now pictures can be dragged from one to another

--- src/data/pictures.js
+++ src/data/pictures.js
@@ -20,3 +20,25 @@ export const picturesArray = [
     src: "https://i.pinimg.com/474x/3d/30/44/3d30444c443c859b88c395b783cbcdee.jpg",
   },
 ];
+export const picturesArray2 = [
+  {
+    id: "5",
+    src: "https://i.pinimg.com/474x/76/18/fb/7618fba92e32a9f15805dd9a52dbd027.jpg",
+  },
+  {
+    id: "6",
+    src: "https://i.pinimg.com/474x/4f/e4/be/4fe4beceea68bb43e8c6a73826d38c74.jpg",
+  },
+  {
+    id: "7",
+    src: "https://i.pinimg.com/474x/40/57/08/4057086e00992997f31b7b7079ed854a.jpg",
+  },
+  {
+    id: "8",
+    src: "https://i.pinimg.com/474x/38/f7/cd/38f7cd9964584b87d3e6c7c2d40552cd.jpg",
+  },
+  {
+    id: "9",
+    src: "https://i.pinimg.com/474x/3d/30/44/3d30444c443c859b88c395b783cbcdee.jpg",
+  },
+];
--- src/store/context/PicturesContext/PicturesContextProvider.js
+++ src/store/context/PicturesContext/PicturesContextProvider.js
@@ -1,10 +1,13 @@
 import PicturesContext from "./PicturesContext";
-import { picturesArray } from "../../../data/pictures";
+import { picturesArray, picturesArray2 } from "../../../data/pictures";
 import { useReducer } from "react";
 import PicturesReducer from "../../reducers/pictures/picturesReducer";
 
 const PicturesContextProvider = ({ children }) => {
-  const [pictures, dispatch] = useReducer(PicturesReducer, picturesArray);
+  const [pictures, dispatch] = useReducer(PicturesReducer, {
+    picturesArray,
+    picturesArray2,
+  });
   const data = { pictures, dispatch };
   return (
     <PicturesContext.Provider value={data}>{children}</PicturesContext.Provider>
